12d690bf637e177b454af1e721a9a91385d1994c
Lauren Smolenski <lauren@smolenski.net.nz>
2020-10-21 08:01:30 +1300

    about and pages design complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+  end
+
+  def eden_essence
+  end
+
+  def breathwork
+  end
+
+  def hero_projects
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'pages#home'
+
+  get 'about' => 'pages#about'
+  get 'eden_essence' => 'pages#eden_essence'
+  get 'breathwork' => 'pages#breathwork'
+  get 'hero_projects' => 'pages#hero_projects'
 end
